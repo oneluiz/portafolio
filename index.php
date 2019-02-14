f9b93a6f4cd1cf6b1eda3275252de34557849550
Luis Cortés <luizcortesj@gmail.com>
2019-02-13 20:00:23 -0600

    update web

---
+++ index.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Luis Cortés | Portfolio</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Luis Cortés | Portfolio">
+    <meta name="keywords" content="cv, onepage, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link href="http://bootex.greatrangefinder.com/frozentheme/demo1/img/favicon.png" rel="shortcut icon" />
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="http://bootex.greatrangefinder.com/frozentheme/demo1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/owl.carousel.css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+
+    <!--====== Header Section Start ======-->
+    <header class="header-section">
+        <div class="logo">
+            <img src="img/logo.png" alt="">
+            <!-- Logo -->
+        </div>
+        <div class="responsive"><i class="fa fa-bars"></i></div>
+        <!-- Navigation -->
+        <ul class="menu-list">
+            <li><a href="#home">Inicio</a></li>
+            <li><a href="#about">Sobre mi</a></li>
+            <li><a href="#service">Servicios</a></li>
+            <li><a href="#portfolio">Portafolio</a></li>
+            <li><a href="#contact">Contáctame</a></li>
+        </ul>
+    </header>
+    <!--====== Header Section End ======-->
+
+
+    <!-- ==== Intro Section Start ==== -->
+    <section class="intro-section fix" id="home">
+        <div class="intro-bg bg-cms"></div>
+        <div class="intro-inner">
+            <div class="intro-content">
+                <div id="round"></div>
+                <div class="profile-img">
+                    <img src="img/avatar.png" alt="">
+                </div>
+                <h2><span class="element"></span></h2>
+            </div>
+        </div>
+    </section>
+    <!-- ==== Intro Section End ==== -->
+
+    <main class="main-warp">
+        <!-- ==== About Section Start ==== -->
+        <section class="about-section fix" id="about">
+            <div class="container pt100 pb100">
+                <div class="row">
+                    <div class="col-md-7 about-text">
+                        <h2>Una historia corta sobre mí.</h2>
+                        <p>Soy Luis Cortés, un Desarrollador Web y diseñador digital con mas de 9 años de experiencia, actualmente trabajo en desarrollo web, medios digitales y community manager. Si tienes un proyecto que necesita una inyección creativa, ¡ahí es donde entro yo!</p>
+                        <p>Mi trabajo es construir su sitio web para que sea funcional, pero al mismo tiempo atractivo. Además, agrego un toque personal a tu producto y me aseguro de que sea llamativo y fácil de usar. Mi objetivo es transmitir su mensaje e identidad de la manera más creativa.</p>
+                    </div>
+                    <div class="col-md-5 skill">
+                        <!-- Skill item -->
+                        <div class="single-progress-item">
+                            <p>Diseño Gráfico</p>
+                            <div class="progress-bar-style" data-progress="100"></div>
+                        </div>
+                        <!-- Skill item -->
+                        <div class="single-progress-item">
+                            <p>Diseño web</p>
+                            <div class="progress-bar-style" data-progress="100"></div>
+                        </div>
+                        <!-- Skill item -->
+                        <div class="single-progress-item">
+                            <p>Frameworks</p>
+                            <div class="progress-bar-style" data-progress="100"></div>
+                        </div>
+                        <!-- Skill item -->
+                        <div class="single-progress-item">
+                            <p>Diseño UX/UI</p>
+                            <div class="progress-bar-style" data-progress="100"></div>
+                        </div>
+                        <!-- Skill item -->
+                        <div class="single-progress-item">
+                            <p>Community Manager</p>
+                            <div class="progress-bar-style" data-progress="100"></div>
+                        </div>
+                        <div class="single-progress-item">
+                            <p>sistema de información geográfica (SIG)</p>
+                            <div class="progress-bar-style" data-progress="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== About Section End ==== -->
+
+
+        <!-- ==== About Section Start ==== -->
+        <section class="service-section fix" id="service">
+            <div class="container pt100 pb100">
+                <div class="stitle mb70">
+                    <h2>¡Qué ofrezco!</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="service-item">
+                            <div class="serv-icon">
+                                <i class="fa fa-desktop"></i>
+                            </div>
+                            <div class="serv-content">
+                                <h4>Gráfico</h4>
+                                <li>Diseño de logo</li>
+                                <li>Desarrollo de marca</li>
+                                <li>Pautas de marca</li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="service-item">
+                            <div class="serv-icon">
+                                <i class="fa fa-facebook"></i>
+                            </div>
+                            <div class="serv-content">
+                                <h4>Community Manager</h4>
+                                <li>Manejo</li>
+                                <li>Estrategia</li>
+                                <li>Asesoría</li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="service-item">
+                            <div class="serv-icon">
+                                <i class="fa fa-codepen"></i>
+                            </div>
+                            <div class="serv-content">
+                                <h4>DESARROLLO</h4>
+                                <li>Sitios Web</li>
+                                <li>Aplicaciones Web</li>
+                                <li>Aplicaciones Móviles</li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 wow fadeInUp" data-wow-delay="0.8s">
+                        <div class="service-item">
+                            <div class="serv-icon">
+                                <i class="fa fa-life-ring"></i>
+                            </div>
+                            <div class="serv-content">
+                                <h4>Soporte Técnico</h4>
+                                <li>Asesoría</li>
+                                <li>Reparación</li>
+                                <li>Mantenimiento</li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== About Section End ==== -->
+
+
+        <!-- ==== Portfolio Section Start ==== -->
+        <section class="portfolio-section fix" id="portfolio">
+            <div class="container pt100 pb100">
+                <div class="stitle mb40">
+                    <h2>Últimos trabajos</h2>
+                    <p>Aquí hay algunos proyectos de diseño recientes. ¿Quieres ver más? <a href="#contact">Envíeme un correo electrónico .</a></p>
+                </div>
+
+                <!--<ul class="portfolio-filter mb40">
+                    <li class="filter" data-filter="*">ALL</li>
+                    <li class="filter" data-filter=".ill">ILLUSTRATIONS</li>
+                    <li class="filter" data-filter=".brand">BRANDING</li>
+                    <li class="filter" data-filter=".fs">FASHION</li>
+                </ul>-->
+                <div class="row isotope_items">
+                    <!-- Work item -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 ill">
+                        <a class="work-item" href="img/1.png">
+                            <img src="img/1.png" alt="">
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>Condovac La Costa</h2>
+                                    <h3>Desarrollo Web / Community Manager / Gráfico</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Work item -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 brand">
+                        <a class="work-item" href="img/2.png">
+                            <img src="img/2.png" alt="">
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>Canal 5 Guanacaste</h2>
+                                    <h3>Desarrollo Web</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Work item -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 fs">
+                        <a class="work-item" href="img/3.png">
+                            <img src="img/3.png" alt="">
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>Template HTML Themeforest.net</h2>
+                                    <h3>Desarrollo Web</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Work item -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 brand">
+                        <a class="work-item" href="img/4.png">
+                            <img src="img/4.png" alt="">
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>UNCADELI</h2>
+                                    <h3>Desarrollo Web / Gráfico</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Work item -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 fs">
+                        <a class="work-item" href="img/5.png">
+                            <img src="img/5.png" alt="">
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>API REST Ministerio de Hacienda Costa Rica</h2>
+                                    <h3>Desarrollo Web / Factura Electronica</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Work item -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 ill">
+                        <a class="work-item" href="img/6.png">
+                            <img src="img/6.png" alt="">
+                            <div class="work-inner">
+                                <div class="work-info">
+                                    <h2>Beach Run Playa Hermosa</h2>
+                                    <h3>Desarrollo Web / Gráfico</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== Portfolio Section End ==== -->
+
+        <section class="promotion-section">
+            <div class="promotion-bg"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 promotion">
+                        <h2>Hagamos algún proyecto genial juntos.</h2>
+                        <a href="#contact" class="site-btn btn-light mt25 wow fadeInUp" data-wow-delay="0.2s">¡Contáctame Ahora!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- ==== Contact Section Start ==== -->
+        <section class="contact-section fix" id="contact">
+            <div class="container pt100 pb100">
+                <div class="stitle mb70">
+                    <h2>Ponte en contacto conmigo</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="row">
+                            <!-- Contact Info -->
+                            <div class="col-md-4 col-sm-4 contact-info wow fadeInUp" data-wow-delay="0.2s">
+                                <i class="fa fa-phone"></i>
+                                <p>+(506) 87969889</p>
+                            </div>
+                            <!-- Contact Info -->
+                            <div class="col-md-4 col-sm-4 contact-info wow fadeInUp" data-wow-delay="0.4s">
+                                <i class="fa fa-map-marker"></i>
+                                <p>50101, Liberia, Guanacaste</p>
+                            </div>
+                            <!-- Contact Info -->
+                            <div class="col-md-4 col-sm-4 contact-info wow fadeInUp" data-wow-delay="0.6s">
+                                <i class="fa fa-send"></i>
+                                <p>lcortes@devfy.com</p>
+                            </div>
+                        </div>
+                        <!-- Contact Form -->
+                        <form action="mail.php" class="cont-form" id="contact-form" method="POST">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <input type="text" name="name" id="name" required placeholder="Su nombre*">
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="email" name="email" id="email" required placeholder="Su Correo*">
+                                </div>
+                                <div class="col-sm-12">
+                                    <textarea id="massage" name="massage" required placeholder="Su Mensaje*"></textarea>
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="site-btn" data-wow-delay="0.2s" id="send-form">Enviar Mensaje</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ==== Contact Section End ==== -->
+    </main>
+
+
+    <!-- ==== Footer Section Start ==== -->
+    <footer class="footer-section fix">
+        <div class="container">
+            <!-- social media -->
+            <div class="social-links mt25">
+                <a href="https://www.facebook.com/oneluiz" class="facebook"><i class="fa fa-facebook"></i></a>
+                <a href="https://twitter.com/Luizcrtz" class="twitter"><i class="fa fa-twitter"></i></a>
+                <a href="https://github.com/oneluiz/" class="behance" style="background:black"><i class="fa fa-github"></i></a>
+            </div>
+            <p>Copyright © <?php echo date('Y'); ?>, Todos los derechos reservados.</p>
+            <p>Creado por Luis Cortés con <i class="fa fa-heart" style="color:red;"></i> &amp; <i class="fa fa-coffee" style="color:brown;"></i></p>
+        </div>
+    </footer>
+    <!-- ==== Footer Section End ==== -->
+
+
+    <!--====== Javascripts & Jquery ======-->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/plugin.js"></script>
+    <script src="js/init-round.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
